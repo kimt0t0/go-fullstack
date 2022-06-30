99ec425b2cefd2b999e00e50e14aa4c81577b083
kimt0t0 <kimagine@laposte.net>
2022-06-30 17:57:42 +0200

    Création serveur et ajout des articles dans l'app Express pour affichage sur la partie front en modifiant les paramètres du CORS

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,51 @@
 const http = require('http'); //importe le package HTTP natif de Node
 const app = require('./app'); //importe l'application fonctionnant avec Express
 
-app.set('port', process.env.PORT || 3000);
-//création d'un serveur avec une fonction exécutée à chaque appel vers ce serveur:
-const server = http.createServer((req, res) => {
-    res.end("Voici la réponse du serveur!"); //réponse du serveur à l'appelant
-});
+const normalizePort = val => {
+    const port = parseInt(val, 10);
 
-server.listen(process.env.PORT || 3000); //le serveur écoute soit le port indiqué par la plateforme, soit port 3000 par défaut
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+//création d'un serveur avec une fonction exécutée à chaque appel vers ce serveur:
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
+    console.log('listening on ' + bind);
+})
+
+server.listen(port); //le serveur écoute soit le port indiqué par la plateforme, soit port 3000 par défaut
 
 //Exécuter 'node server' depuis la console pour démarrer le serveur!
