f143e4457d518df8efea27dfdc15688970595c04
kimt0t0 <kimagine@laposte.net>
2022-06-30 17:59:11 +0200

    Création du serveur et ajout des articles via l'app Express avec modification des autorisations CORS

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,33 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next(); //renvoie vers le prochain middleware
-});
+    res.setHeader('Access-Control-Allow-Origin', '*'); //permet d'accéder à l'API depuis n'importe-quelle origine
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); //ajout des headers aux requêtes envoyées
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); //envoi des requêtes avec les méthodes mentionnées
+    next();
+  });
 
-app.use((req, res, next) => {
-    res.json({
-        message: "Votre requête a bien été reçue!"
-    });
-});
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 
 module.exports = app;
--- backend/dist/server.dev.js
+++ backend/dist/server.dev.js
@@ -6,11 +6,55 @@ var http = require('http'); //importe le package HTTP natif de Node
 var app = require('./app'); //importe l'application fonctionnant avec Express
 
 
-app.set('port', process.env.PORT || 3000); //création d'un serveur avec une fonction exécutée à chaque appel vers ce serveur:
+var normalizePort = function normalizePort(val) {
+  var port = parseInt(val, 10);
 
-var server = http.createServer(function (req, res) {
-  res.end("Voici la réponse du serveur!"); //réponse du serveur à l'appelant
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var errorHandler = function errorHandler(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var address = server.address();
+  var bind = typeof address === 'string' ? 'pipe' + address : 'port: ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+
+    default:
+      throw error;
+  }
+}; //création d'un serveur avec une fonction exécutée à chaque appel vers ce serveur:
+
+
+var server = http.createServer(app);
+server.on('error', errorHandler);
+server.on('listening', function () {
+  var address = server.address();
+  var bind = typeof address === 'string' ? 'pipe' + address : 'port' + port;
+  console.log('listening on ' + bind);
 });
-server.listen(process.env.PORT || 3000); //le serveur écoute soit le port indiqué par la plateforme, soit port 3000 par défaut
+server.listen(port); //le serveur écoute soit le port indiqué par la plateforme, soit port 3000 par défaut
 //Exécuter 'node server' depuis la console pour démarrer le serveur!
 //# sourceMappingURL=server.dev.js.map
